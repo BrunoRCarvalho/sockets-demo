b6a57ca252f6bec551fb852a533ee806ad468d8b
Jonathan May <jmay4826@gmail.com>
2018-02-05 21:14:11 -0600

    basic functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,59 +2,102 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import openSocket from "socket.io-client";
-const socket = openSocket();
+// const socket = openSocket();
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { a: 1, b: 1, users: [] };
-    socket.on("update votes", ({ a, b }) => {
-      this.setState({ a, b });
+    this.state = {
+      question: {
+        id: 0,
+        text: "",
+        a: 0,
+        b: 0
+      },
+      users: []
+    };
+    this.next = this.next.bind(this);
+  }
+  componentDidMount() {
+    this.socket = openSocket();
+    this.socket.on("new question", question => this.setState({ question }));
+    this.socket.on("update votes", question => {
+      this.setState({ question });
     });
-    socket.on("new user", users => this.setState({ users }));
+    this.socket.on("new user", users => this.setState({ users }));
   }
   vote(choice) {
-    socket.emit("vote", choice);
+    this.socket.emit("vote", choice);
+    this.setState({ voted: true });
+  }
+  next() {
+    this.socket.emit("next question");
   }
   render() {
-    const { a, b } = this.state;
+    const isAdmin = !!window.location.href.split("?")[1];
+    const styles = {
+      voteContainer: {
+        display: "flex",
+        fledDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "flex-end",
+        height: "300px",
+        borderBottom: "1px solid black"
+      },
+      bar: {
+        transition: "all 500ms ease-in-out",
+        width: "100px",
+        borderTopLeftRadius: "8px",
+        borderTopRightRadius: "8px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        color: "#000000"
+      }
+    };
+    console.log(this.state);
+    console.log(isAdmin);
+    const { a, b } = this.state.question;
+    const aPercent = Math.floor(a / (a + b) * 100) || 0;
+    const bPercent = Math.floor(b / (a + b) * 100) || 0;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h1>{this.state.users.length}</h1>
-        <button onClick={() => this.vote("a")}>a</button>
-        <button onClick={() => this.vote("b")}>b</button>
+        <p>{this.state.question.text}</p>
+        <button onClick={() => this.vote("a")}>True</button>
+        <button onClick={() => this.vote("b")}>False</button>
+
         <p>
-          {this.state.a} A - {this.state.b} B
+          {a} A - {b} B
         </p>
-        <div
-          style={{
-            width: "60%"
-          }}
-        >
+
+        <div style={styles.voteContainer}>
           <div
             style={{
-              width: `${a / (a + b) * 100}%`,
-              height: "100px",
-              background: "blue",
-              transition: "all 500ms ease-in-out"
+              ...styles.bar,
+              height: `${aPercent}%`,
+              background: "#E44424"
             }}
-          />
+          >
+            <p>True</p>
+            <p>{aPercent}%</p>
+            <p>{a} votes</p>
+          </div>
           <div
             style={{
-              width: `${b / (a + b) * 100}%`,
-              transition: "all 500ms ease-in-out",
-              height: "100px",
-              background: "red"
+              ...styles.bar,
+              height: `${bPercent}%`,
+              background: "#A2AB58"
             }}
-          />
+          >
+            <p>False</p>
+            <p>{bPercent}%</p>
+            <p>{b} votes</p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.users.length}</h1>
+        <h3>{this.state.users.length === 1 ? "user" : "users"} online</h3>
+        <button onClick={this.next}>Next Question</button>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -3,28 +3,25 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-let db;
-
-if (!process.env.PORT) {
-  let use = `${__dirname}/../client/build`;
-  console.log(use);
-  app.use(express.static(use));
-}
-app.get("/api/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
 
 let pollResults = { a: 0, b: 0 };
 let users = [];
+let questions = [
+  { id: 0, text: "True or False: I like Kahoots", a: 0, b: 0 },
+  { id: 1, text: "True or False: I like Javascript", a: 0, b: 0 },
+  { id: 2, text: "True or False: I like Assessments", a: 0, b: 0 }
+];
+
+let currentQuestion = 0;
 
 io.on("connection", socket => {
-  console.log("connected");
+  console.log("connected", questions[currentQuestion]);
   users.push(socket.id);
   io.emit("new user", users);
   //   socket.emit("new user", users);
   //   socket.broadcast.emit("new user", users);
   //   socket.emit("update votes", pollResults);
-  io.emit("update votes", pollResults);
+  io.emit("update votes", questions[currentQuestion]);
 
   socket.on("disconnect", reason => {
     console.log(reason);
@@ -33,13 +30,22 @@ io.on("connection", socket => {
   });
 
   socket.on("vote", vote => {
-    pollResults[vote]++;
-    console.log(vote, pollResults);
+    console.log(vote);
+    questions[currentQuestion][vote]++;
     // socket.emit("update votes", pollResults);
-    io.emit("update votes", pollResults);
+    io.emit("update votes", questions[currentQuestion]);
+  });
+  socket.on("next question", () => {
+    if (currentQuestion < questions.length - 1) {
+      currentQuestion++;
+    } else {
+      currentQuestion = 0;
+    }
+
+    io.emit("update votes", questions[currentQuestion]);
   });
 });
 
-http.listen(process.env.PORT || 80, () => {
-  console.log(`listening on ${process.env.PORT || 80}`);
+http.listen(5000, () => {
+  console.log(`listening on 5000`);
 });
